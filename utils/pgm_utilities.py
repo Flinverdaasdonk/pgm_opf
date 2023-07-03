8dd62cff50326d40bb23784fc051694576f16161
Flinverdaasdonk <flin.verdaasdonk@gmail.com>
2023-07-03 16:01:44 +0200

    add congestion quantification utility

--- utils/pgm_utilities.py
+++ utils/pgm_utilities.py
@@ -2,9 +2,7 @@ from power_grid_model import LoadGenType
 from power_grid_model import PowerGridModel, CalculationMethod, CalculationType
 from power_grid_model import initialize_array
 from power_grid_model.validation import assert_valid_input_data
-
-
-
+import numpy as np
 
 def network_to_pgm_format(network, validate_pgm_input=True):
     # these keys should be taken from the network data in opf format, and added to the pgm input_data
@@ -34,6 +32,20 @@ def network_to_pgm_format(network, validate_pgm_input=True):
         assert_valid_input_data(input_data=pgm_input_data, calculation_type=CalculationType.power_flow)
 
     return pgm_input_data
+
+def quantify_congestion(pgm_output, network):
+    """
+    Quantify the congestion given the presented pgm_output and networks
+    """
+    line_limits = network["line"]["i_max"]
+    line_currents = pgm_output["line"]["i_from"] # CHECK IF I_FROM IS THE CORRECT ONE; WHY NOT I_TO?
+    
+    line_congestion_func = lambda i, i_max : np.max([0, np.abs(i)-i_max])**2
+
+    net_congestion = np.sum([line_congestion_func(i=i, i_max=i_max) for i, i_max in zip(line_currents, line_limits)])
+
+    return net_congestion
+
 
 if __name__ == "__main__":
     pass
